6e00890aebc71c1b004f9c983d096dec3983b26c
Taketake2145 <simplesimplesdesign@gmail.com>
2026-04-01 05:40:11 +0900

    commonファイルを調整する

--- www/functions/common/getGalleryImages.php
+++ www/functions/common/getGalleryImages.php
@@ -1,9 +1,10 @@
 <?php
 /**
- * jpeg画像の一覧を取得する
+ * jpeg画像の一覧を配列で取得する
+ * 対象は、指定フォルダ内のサブフォルダも対象とする
  *
- * $dir サーバーのフルパス e.g. /var/www/html/assets/gallery/
- * $trim サーバーのフルパスから公開パスを生成するための文字列 e.g /var/www/html → /assets/gallery/
+ * $dir 指定フォルダのサーバーフルパス e.g. /var/www/html/assets/gallery/
+ * $trim サーバーのフルパスから公開パスを生成するための文字列 e.g /var/www/html をセットした場合の返却は /assets/gallery/
  */
 function getGalleryImages($dir, $trim = '') {
   $result = [];
